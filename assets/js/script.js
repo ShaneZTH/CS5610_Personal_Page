0a1a38a9f0a0c0d29179bb03833cfca8d0794b10
Shane Z <22213632+ShaneZTH@users.noreply.github.com>
2022-09-26 13:06:59 -0700

    Added Vanilla JS feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,35 @@
-$(document).ready(function () {
-    $(window).on("load",function(){
-      $("#loader-wrapper").fadeOut("slow");
+/*--------------------------------------------------------------
+// JQuery Version
+--------------------------------------------------------------*/
+// $(document).ready(function () {
+//     $(window).on("load",function(){
+//       $("#loader-wrapper").fadeOut("slow");
+//     });
+
+//     $(".bi-arrow-up-short").click(function(){
+//        $("html, body").animate({ scrollTop: 0 }, 600);
+//     });
+// });
+
+
+/*--------------------------------------------------------------
+// Vanilla JS Version
+--------------------------------------------------------------*/
+
+// Loader fadeOut
+window.addEventListener('load', function () {
+  let loader = document.getElementById('loader-wrapper');
+  loader.classList.add('fadeOut');
+}, false);
+
+
+// Scroll Top
+const scrollTopBtn = document.querySelector(".bi-arrow-up-short");
+scrollTopBtn.addEventListener('onclick', ()=>{
+  window.scrollTo({
+        top: 0,
+        behavior: "smooth"
     });
-
-
-    $(".bi-arrow-up-short").click(function(){
-       $("html, body").animate({ scrollTop: 0 }, 600);
-    });
-
-});
+}, false);
 
 
