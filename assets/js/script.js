adb5a4486bd2fdfb7e259bed9ef00c76ed017f5b
Shane Z <22213632+ShaneZTH@users.noreply.github.com>
2022-09-26 13:17:10 -0700

    Formatted via JSPrettier

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,25 +11,29 @@
 //     });
 // });
 
-
 /*--------------------------------------------------------------
 // Vanilla JS Version
 --------------------------------------------------------------*/
 
 // Loader fadeOut
-window.addEventListener('load', function () {
-  let loader = document.getElementById('loader-wrapper');
-  loader.classList.add('fadeOut');
-}, false);
-
+window.addEventListener(
+  "load",
+  function () {
+    let loader = document.getElementById("loader-wrapper");
+    loader.classList.add("fadeOut");
+  },
+  false
+);
 
 // Scroll Top
 const scrollTopBtn = document.querySelector(".bi-arrow-up-short");
-scrollTopBtn.addEventListener('onclick', ()=>{
-  window.scrollTo({
-        top: 0,
-        behavior: "smooth"
+scrollTopBtn.addEventListener(
+  "onclick",
+  () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
     });
-}, false);
-
-
+  },
+  false
+);
